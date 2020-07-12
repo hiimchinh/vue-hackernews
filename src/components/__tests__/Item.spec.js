c00ece51f9494e209d3e3c11d9a2ad8230738922
hiimchinh <chinhhm@stringee.com>
2020-07-12 21:19:18 +0700

    add test case for item url

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -5,9 +5,25 @@ describe('Item.vue', () => {
   test('sanity test', () => {
     console.log(Item)
   })
-  test("renders 'item'", () => {
-    const wrapper = shallowMount(Item)
-    debugger
-    expect(wrapper.text()).toContain('item')
+  test('renders item.url', () => {
+    const item = {
+      url: 10
+    }
+    const wrapper = shallowMount(Item, {
+      propsData: { item }
+    })
+    expect(wrapper.text()).toContain(item.url)
+  })
+  test('renders a link to the item.url with item.title as text', () => {
+    const item = {
+      url: 'http://some-url.com',
+      title: 'some title'
+    }
+    const wrapper = shallowMount(Item, {
+      propsData: { item }
+    })
+    const a = wrapper.find('a')
+    expect(a.text()).toBe(item.title)
+    expect(a.attributes().href).toBe(item.url)
   })
 })
