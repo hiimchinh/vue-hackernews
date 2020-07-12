d8c2e422ff4c6bc787f14f6a6a87987c602ccb4b
hiimchinh <chinhhm@stringee.com>
2020-07-12 16:12:55 +0700

    using shallowMount with text function

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,12 +1,12 @@
 import Item from '../Item.vue'
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
 describe('Item.vue', () => {
   test('sanity test', () => {
     console.log(Item)
   })
   test("renders 'item'", () => {
-    const wrapper = mount(Item)
-    expect(wrapper.vm.$el.textContent).toContain('item')
+    const wrapper = shallowMount(Item)
+    expect(wrapper.text()).toContain('item')
   })
 })
