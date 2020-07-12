3897835ba3d4a9a82dfebd4e141cf88c65ec1e77
hiimchinh <chinhhm@stringee.com>
2020-07-12 12:30:50 +0700

    add render function for Item.spec.js

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -5,4 +5,9 @@ describe('Item.vue', () => {
   test('sanity test', () => {
     console.log(Item)
   })
+  test("renders 'item'", () => {
+    const Ctor = Vue.extend(Item)
+    const vm = new Ctor().$mount()
+    expect(vm.$el.textContent).toContain('item')
+  })
 })
